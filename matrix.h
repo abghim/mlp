7a68dbac3545528dadd1d454eb3de5bc5789f5a3
Aiden Ghim <aiden.bj.ghim@gmail.com>
2025-05-06 08:55:41 +0900

    Add header file for matrix

---
+++ matrix.h
@@ -0,0 +1,22 @@
+#ifndef _MATRIX_H_
+#define _MATRIX_H_
+
+#define MAXMAT 100
+
+typedef struct {
+    unsigned int row;
+    unsigned int col;
+    double *data;
+} matrix;
+
+
+
+matrix *matnew(int row, int col);
+double matget(matrix *target, int row, int col);
+void matput(matrix *target, int row, int col, double val);
+void matmuleq(matrix *to, matrix *with);
+void mataddeq(matrix *to, matrix *with);
+void matfree();
+
+
+#endif
