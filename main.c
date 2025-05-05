50905fe718088c1461750242dd50d41f7207b547
Aiden Ghim <aiden.bj.ghim@gmail.com>
2025-05-06 08:54:49 +0900

    Add mlp code

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "matrix.h"
+
+static void matprint(const matrix *m, const char *name)
+{
+    printf("%s (%zux%zu):\n", name, m->row, m->col);
+    for (size_t r = 0; r < m->row; ++r) {
+        for (size_t c = 0; c < m->col; ++c)
+            printf("%8.2f ", matget(m, r, c));
+        putchar('\n');
+    } putchar('\n');
+}
+
+int main()
+{
+	return 0;
+}
+
